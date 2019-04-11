1cc6749863c3bc6715e74aed6b2ac581af1c02fb
jehillert <john.hillert@gmail.com>
2019-04-11 07:01:28 -0500

    Checkpoint: Testing minor change to CardColumn

--- src/components/card-column/CardColumn.jsx
+++ src/components/card-column/CardColumn.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/forbid-prop-types */
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
