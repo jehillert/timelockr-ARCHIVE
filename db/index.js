311f126df3ee600f557b4d59141d4ae1af572275
jehillert <john.hillert@gmail.com>
2019-04-24 06:39:02 -0500

    same

--- db/index.js
+++ db/index.js
@@ -5,29 +5,30 @@ const c = require('../server/helpers/chalks');
 const chalk = require('chalk');
 const Promise = require('bluebird');
 
-const initOptions = { promiseLib: Promise };
-const pgp = require('pg-promise')(initOptions);
 const debug = require('debug')(c.blueBlack('database:index'));
-
 debug(c.error(' DEVELOPMENT MODE '), chalk.inverse(' Debugging Enabled '));
 
-const config = {
-  user               : process.env.PGUSER,
-  password           : process.env.PGPASSWORD,
-  database           : process.env.PGDATABASE || 'db',
-  host               : process.env.PGHOST || 'localhost',
-  port               : process.env.PGPORT || 5432,
-  max                : process.env.PG_MAX || 100,
-  idleTimeoutMillis  : process.env.PG_IDLETIMEOUTMILLIS || 30000,
-  ssl                : process.env.PG_SSL || true,
-};
+const initOptions = { promiseLib: Promise };
+const pgp = require('pg-promise')(initOptions);
 
-debug(config);
-
-const db = pgp(config);
+const connectionString = process.env.DATABASE_URL
+const db = pgp(connectionString);
 
 module.exports = db;
-/*
-Ubuntu Configuration:
-  https://help.ubuntu.com/stable/serverguide/postgresql.html
-*/
+
+// const config = {
+//   user               : process.env.PGUSER,
+//   password           : process.env.PGPASSWORD,
+//   database           : process.env.PGDATABASE || 'db',
+//   host               : process.env.PGHOST || 'localhost',
+//   port               : process.env.PGPORT || 5432,
+//   max                : process.env.PG_MAX || 100,
+//   idleTimeoutMillis  : process.env.PG_IDLETIMEOUTMILLIS || 30000,
+//   ssl                : process.env.PG_SSL || true,
+// };
+//
+// debug(config);
+//
+// const db = pgp(config);
+//
+// module.exports = db;
